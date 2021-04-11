ce34ffb39a32ad9c834ee43e7d520aebbc6146ff
Hang Yang <yangdehang0204@gmail.com>
2021-04-11 14:49:35 -0400

    clean up

--- spark_demo/rdd/wordcount.py
+++ spark_demo/rdd/wordcount.py
@@ -1,6 +1,19 @@
-from pyspark import SparkContext
+from pyspark import SparkContext,SparkConf 
+import os
 
+os.environ["PYSPARK_PYTHON"] = "C:\Python\python"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "C:\Python\python"
 if __name__ == '__main__':
-    sc = SparkContext("local[3]","word count")
+    conf = SparkConf().setAppName("word count").setMaster("local[3]")
+    sc = SparkContext(conf = conf)
     sc.setLogLevel("ERROR");
+
+    lines = sc.textFile("in/word_count.text")
+
+    words = lines.flatMap(lambda line: line.split(" "))
+
+    wordCounts = words.countByValue()
     
+    for key in wordCounts:
+        print(key, " : ", wordCounts[key])
+
